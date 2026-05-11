f6116c05e11e0f4068cb925521dfee1788ddc8ef
Noah <noahgonzales281@gmail.com>
2026-05-10 21:48:51 -0700

    update login screen quote font size

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="login-overlay" />
         <div className="quote">
           <Shield size={28} />
-          <p>Thanks to the help of CSRP Utilities, we've been able to add features specific to our server that no other bot could offer, like an advanced infractions system, fast staff management commands like /reinstate, and the addition of our smart hits system which automatically reviews hits and sends them for a secondary staff review.</p>
+          <h2>Thanks to the help of CSRP Utilities, we've been able to add features specific to our server that no other bot could offer, like an advanced infractions system, fast staff management commands like /reinstate, and the addition of our smart hits system which automatically reviews hits and sends them for a secondary staff review.</h2>
           <span>- PointlessTalderson (Director)</span>
         </div>
       </section>
